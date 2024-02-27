ae935c470eab6a095ef09dcc716b2fe56a4c589e
shubham <shubhsonkar777@gmail.com>
2024-02-27 10:54:57 -0800

    ThreadView

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { SideBarWidget } from './widgets/sidebar-widget';
 import { GLOBAL } from './utils/global-data';
 import { CONTEXT } from './controls/sidebar';
 import Menu from './menu/menu';
+import { composeThreadView } from './controls/threadView';
+/* import { composeThreadView } from './controls/threadView'; */
+
 
 class Main {
     constructor() {
@@ -13,16 +16,19 @@ class Main {
 
     init() {
       console.log(ASCII_ART);
-      InboxSDK.load(2, "sdk_ChatGpt4Gmail_580a70a147", {}).then((sdk) => {
+      InboxSDK.load(2, "sdk_ChatGpt4Gmail_580 a70a147", {}).then((sdk) => {
         GLOBAL.sdk = sdk;
         // the SDK has been loaded, now do something with it!
         GLOBAL.sdk.Compose.registerComposeViewHandler((composeView) => {
           GLOBAL.composeView = composeView;
           GLOBAL.composeView.addButton(composeMenuDD());
         });
-
+        GLOBAL.sdk.Conversations.registerThreadViewHandler((threadView) => {
+          composeThreadView(threadView);
+          
+        })
         // TO add global sidebar
-        SideBarWidget(CONTEXT.THREAD, Menu.MENU_TYPE.NAVIGATION_MAIN);
+        //SideBarWidget(CONTEXT.THREAD, Menu.MENU_TYPE.NAVIGATION_MAIN);
       });
     }
 }
--- src/utils/global-data.ts
+++ src/utils/global-data.ts
@@ -4,6 +4,7 @@ export class GlobalData {
   contentPanelRef: InboxSDK.ContentPanelView | null = null;
   sdk: InboxSDK.InboxSDK | null = null;
   composeView: InboxSDK.ComposeView | null = null;
+  threadView: InboxSDK.ThreadView | null = null;
   appName: string = "ChatGpt4Gmail";
   version = "v0.1";
   cgApiKey: string = "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";
